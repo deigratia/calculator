54f77b75e0944bda5336b6ef23fefd169d5a1aa6
Dei Gratia R R <rdeigratia95@gmail.com>
2018-04-02 16:31:18 +0700

    modiefied dom.js and index.html

--- dom.js
+++ dom.js
@@ -11,6 +11,7 @@ function CalculatorAdd (){
   number2 = Number(value2);
   let result = number1 + number2;
   console.log(result);
+  document.getElementById('addoutput').innerHTML = result;
   return result;
 }
 
@@ -26,9 +27,41 @@ function Calculatormultiply (){
   number2 = Number(value2);
   let result = number1 * number2;
   console.log(result);
+  document.getElementById('mutoutput').innerHTML = result;
   return result;
 }
 
+
+function Calculatordivide (){
+  let kolom5 = document.getElementById('kolom5');
+  let kolom6 = document.getElementById('kolom6');
+
+  let value1 = kolom5.value
+  let value2 = kolom6.value
+
+  number1 = Number(value1);
+  number2 = Number(value2);
+  let result = number1 / number2;
+  console.log(result);
+  document.getElementById('divoutput').innerHTML = result;
+  return result;
+}
+
+
+function Calculatorsubstrack(){
+  let kolom7 = document.getElementById('kolom7');
+  let kolom8 = document.getElementById('kolom8');
+
+  let value1 = kolom7.value
+  let value2 = kolom8.value
+
+  number1 = Number(value1);
+  number2 = Number(value2);
+  let result = number1 - number2;
+  console.log(result);
+  document.getElementById('suboutput').innerHTML = result;
+  return result;
+}
 
 // let finalresult = CalculatorAdd(kolom1.value,kolom2.value)
 // console.log(finalresult);
